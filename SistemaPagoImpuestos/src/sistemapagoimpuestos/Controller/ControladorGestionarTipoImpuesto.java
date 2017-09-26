a138de34d557db154a9cdf55adbdf7120f9390e3
Marcos E. Luna <lunamarcose@gmail.com>
2017-09-26 20:09:24 -0300

    Corregido Gestionar Tipo Impuesto, solamente se crea esta entidad

--- SistemaPagoImpuestos/src/sistemapagoimpuestos/Controller/ControladorGestionarTipoImpuesto.java
+++ SistemaPagoImpuestos/src/sistemapagoimpuestos/Controller/ControladorGestionarTipoImpuesto.java
@@ -39,14 +39,10 @@ public class ControladorGestionarTipoImpuesto {
     // Metodo nuevoTipoImpuesto (crea un tipoImpuesto)
     public void nuevoTipoImpuesto(int codigoTipoImpuestoIngres, String nombreTipoImpuestoIngres, boolean esMontoEditableIngres, List<DTOEmpresaTipImpItem> dTOEmpresaTipImpItems){
         experto.nuevoTipoImpuesto(codigoTipoImpuestoIngres, nombreTipoImpuestoIngres, esMontoEditableIngres);
-        experto.nuevaEmpresaTipoImpuesto(nombreTipoImpuestoIngres, dTOEmpresaTipImpItems);
-        experto.nuevaEmpresaTipoImpuestoItem(nombreTipoImpuestoIngres, dTOEmpresaTipImpItems);
     }
     //Metodo para modificar TipoImpuesto
     public void modificarTipoImpuesto(String nombreTipoImpuestoIngres, String nombreActualTipoImpuesto, boolean esMontoEditableIngres, boolean habilitado,List<DTOEmpresaTipImpItem> dTOEmpresaTipImpItems){
         experto.modificarTipoImpuesto(nombreTipoImpuestoIngres, nombreActualTipoImpuesto, esMontoEditableIngres, habilitado);
-        experto.nuevaEmpresaTipoImpuesto(nombreTipoImpuestoIngres, dTOEmpresaTipImpItems);
-        experto.nuevaEmpresaTipoImpuestoItem(nombreTipoImpuestoIngres, dTOEmpresaTipImpItems);
         
     }
     // Metodo para recuperar todos los TipoImpuesto de la DB Que devuelve????
@@ -62,7 +58,6 @@ public class ControladorGestionarTipoImpuesto {
                 pantallaAlta.setVisible(true); // La hago visible
                 // Modifico la operación de cierre para volver a la pantalla principal
                 pantallaAlta.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-                pantallaAlta.generarEmpresaItems();
                 IUGestionarTipoImpuestoAlta.setDtoetiisModfAlta(new ArrayList<DTOEmpresaTipImpItem>());
                 pantallaAlta.addWindowListener(new WindowAdapter() {
                     public void windowClosing(WindowEvent ev) {
@@ -86,8 +81,6 @@ public class ControladorGestionarTipoImpuesto {
                             iniciar();
                         }
                     });
-                    pantallaModificar.obtenerEmpresaItems(dtoTi.getdTOEmpresaTipoImpuestoItemList());
-                    pantallaModificar.RecuperarEmpresaItems();
                     pantallaModificar.setNombre_actual(dtoTi.getNombreDTOTipoImpuesto());
                     pantallaModificar.setTextfield_nombre(dtoTi.getNombreDTOTipoImpuesto());
                     pantallaModificar.setCheckbox_esEditable(dtoTi.isEsMontoEditableDTOTipoImpuesto());
@@ -105,24 +98,10 @@ public class ControladorGestionarTipoImpuesto {
         }
     }
     
-    public ArrayList<DTOEmpresaItem> obtenerEmpresaItem(){
-        return experto.obtenerEmpresaItem();
-    }
     
     // Metodo para recuperar el TipoImpuesto a modificar
     public DTOTipoImpuesto obtenerTipoImpuesto(int codigo){
         return experto.obtenerTipoImpuesto(codigo);
     }
-    
-    public List<DTOEmpresa> buscarEmpresas(){
-        return experto.buscarEmpresas();
-    }
-    
-    public List<DTOItem> buscarItems(){
-        return experto.buscarItems();
-    }
-    
-    public List<DTOTipoEmpresa> buscarTipoEmpresa(){
-        return experto.buscarTipoEmpresa();
-    }
+
 }

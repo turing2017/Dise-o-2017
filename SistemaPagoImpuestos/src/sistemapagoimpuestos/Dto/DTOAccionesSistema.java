b46f0502e9a6269de2f6ab7e3b9216a9fe53edef
Millo\Mariana <mpenesi@gmail.com>
2017-10-22 09:01:27 -0300

    Se modificó y se dejo en versión "casi final" el logs del calcular liquidaciones. Se modificó código separando en dos funciones por un lado la parte del recálculo de liquidaciones anuladas y por otro la parte del cálculo y generación de nuevas liquidaciones. Se modificó la impresión del log.

--- SistemaPagoImpuestos/src/sistemapagoimpuestos/Dto/DTOAccionesSistema.java
+++ SistemaPagoImpuestos/src/sistemapagoimpuestos/Dto/DTOAccionesSistema.java
@@ -13,49 +13,80 @@ import java.util.logging.Logger;
  * @author ggiorgi
  */
 public class DTOAccionesSistema {
-    private Date fechaHoraAccion;
+
+    private Integer nivel;
     private String accion;
     private String descripcionAccion;
+    private Date fechaHoraAccion;
 
-    public DTOAccionesSistema(String accion, String descripcionAccion, Date fechaHoraAccion) {
+    public DTOAccionesSistema(Integer nivel, String accion, String descripcionAccion, Date fechaHoraAccion) {
+        this.nivel = nivel;
         this.accion = accion;
         this.descripcionAccion = descripcionAccion;
         this.fechaHoraAccion = fechaHoraAccion;
     }
-      
-    public Date getFechaHoraAccion() {
-        return fechaHoraAccion;
-    }
 
-    public void setFechaHoraAccion(Date fechaHoraAccion) {
-        this.fechaHoraAccion = fechaHoraAccion;
+    //Getters
+    public Integer getNivel() {
+        return nivel;
     }
 
     public String getAccion() {
         return accion;
     }
 
-    public void setAccion(String accion) {
-        this.accion = accion;
-    }
-
     public String getDescripcionAccion() {
         return descripcionAccion;
+    }
+
+    public Date getFechaHoraAccion() {
+        return fechaHoraAccion;
+    }
+
+    //Setters
+    public void setNivel(Integer nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setAccion(String accion) {
+        this.accion = accion;
     }
 
     public void setDescripcionAccion(String descripcionAccion) {
         this.descripcionAccion = descripcionAccion;
     }
 
+    public void setFechaHoraAccion(Date fechaHoraAccion) {
+        this.fechaHoraAccion = fechaHoraAccion;
+    }
+
+    //Others
+    /**
+     * Imprime las acciones realizadas en el calcular liquidación
+     */
     public void imprimirSTD() {
-        System.out.println("-------------------------");
-        System.out.println(fechaHoraAccion);
-        System.out.println(accion);
+
+        String separador1 = "", separador2 = "";
+
         System.out.println();
+
+        for (int i = 0; i <= nivel; i++) {
+            separador1 += "-----";
+            separador2 += "     ";
+        }
+        System.out.println(fechaHoraAccion + " " + separador1 + " " + accion.toUpperCase());
+//        System.out.println(String.format("%"+ nivel +"s", accion.toUpperCase()));
+//        System.out.println(String.format("%"+ nivel + 5 +"s", "----------"));
+//        System.out.println(accion.toUpperCase());
+        System.out.println(separador1);
         String[] lineasDescripcion = descripcionAccion.split("\\r?\\n");
         for (String linea : lineasDescripcion) {
-            System.out.println(linea);
+//            System.out.println(String.format("%"+ nivel + 5 +"s", linea));
+            if (!linea.equals("")) {
+                System.out.println(separador2 + " " + linea);
+            }
         }
-        System.out.println("-------------------------");
+        System.out.println(separador1);
     }
+
 }

1de2d90fcaf963886b5e5027b795f12ac276f890
Millo\Mariana <mpenesi@gmail.com>
2017-10-24 19:40:31 -0300

    Main para sistema y para el calcular liquidaciones (el del calcular liquidaciones está comentado)

--- SistemaPagoImpuestos/src/sistemapagoimpuestos/SistemaPagoImpuestos.java
+++ SistemaPagoImpuestos/src/sistemapagoimpuestos/SistemaPagoImpuestos.java
@@ -6,7 +6,31 @@ import sistemapagoimpuestos.View.Login.IULogin;
 public class SistemaPagoImpuestos {
   
     public static void main(String[] args) {
+        
         IULogin iULogin = new IULogin();
-        iULogin.setVisible(true);
+        iULogin.setVisible(true);  
+        
     }
+
 }
+
+//
+//package sistemapagoimpuestos;
+//
+//import java.util.ArrayList;
+//import sistemapagoimpuestos.Controller.ControladorCalcularLiquidaciones;
+//import sistemapagoimpuestos.Dto.DTOAccionesSistema;
+//
+//
+//public class SistemaPagoImpuestos {
+//  
+//    public static void main(String[] args) {
+//        
+//        ControladorCalcularLiquidaciones controlador = new ControladorCalcularLiquidaciones();
+//        ArrayList<DTOAccionesSistema> dtosAccionesSistema = new ArrayList<>();
+//        controlador.iniciar(dtosAccionesSistema);
+//        for (int i = 0; i < dtosAccionesSistema.size(); i++) {
+//            dtosAccionesSistema.get(i).imprimirSTD();   
+//        }
+//    }
+//}
